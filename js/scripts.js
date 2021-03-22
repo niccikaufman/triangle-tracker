a74bc2bd4ba634f53bfff2c5ece7d5c65ac1ca23
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-22 16:18:26 -0700

    add initial functions and input form

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("#confirm").click(function(){
+    const side1Input = $("input#side1").val;
+    const side2Input = $("input#side2").val;
+    const side3Input = $("input#side3").val;
+
+
+  // } else {
+  //     alert("That is not a triangle");
+  // }
+  //   event.preventDefault();
+  });
+});
