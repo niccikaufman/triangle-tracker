aa35afce0b7fd333896258d169081e9de21af6be
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-22 20:03:04 -0700

    break

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
   $("#confirm").click(function(){
-    const side1Input = $("input#side1").val;
-    const side2Input = $("input#side2").val;
-    const side3Input = $("input#side3").val;
+    const side1Input = parseFloat($("input#side1")).val;
+    const side2Input = parseFloat($("input#side2")).val;
+    const side3Input = parseFloat($("input#side3")).val;
+    let result;
 
-
-  // } else {
-  //     alert("That is not a triangle");
-  // }
+    if (side1Input === side2Input && side1Input === side3Input && side2Input === side3Input) {
+      result = $("#result").text("equilateral");
+    } else if (side1Input !== side2Input && side1Input !== side3Input && side2Input !== side3Input) {
+      result = $("#result").text("scalene");
+    } else if (side1Input === side2Input || side3Input === side2Input || side1Input === side3Input) {
+      result = $("#result").text("isosceles");
+    }
     event.preventDefault();
     $("#result-card").toggle();
   });
